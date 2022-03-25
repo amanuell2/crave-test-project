cdd2b64c7e4dad75ab0d627ce1b73e10f8893c22
amanuel <amanuel0000@gmail.com>
2022-03-25 23:32:19 +0300

    task number index font size

--- src/Components/TaskListCard.js
+++ src/Components/TaskListCard.js
@@ -5,8 +5,8 @@ import TaskListItem from "./TaskListItem";
 const TaskListCard = ({ index, id, title, isLocked, subTask }) => {
   return (
     <div className="w-64 bg-white rounded-sm px-1 my-2">
-      <div className="flex h-12 justify-between items-center my-2">
-        <span className="bg-black rounded-full w-6 h-6 text-white text-center font-semibold">
+      <div className="flex h-12 justify-between items-center my-1">
+        <span className="bg-black rounded-full w-6 h-6 text-white text-center text-md font-normal">
           {index + 1}
         </span>
         <h1 className="font-semibold text-md flex-1 mx-2">{title}</h1>
